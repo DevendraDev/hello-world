c7a202a4b5372b46e8fd477b4f94a58d983d2e13
DevendraDev <devendradeotale@yahoo.com>
2022-11-23 16:40:26 +0530

    Update prWithOptions.js

--- prWithOptions.js
+++ prWithOptions.js
@@ -192,11 +192,8 @@ function createPaymentRequest(bDirectApp, sAppUrl){
   paymentRequestGPay  = new PaymentRequest(supportedInstrumentGPay, transactionDetailsGPay);
   paymentRequestPhonepeStage && paymentRequestPhonepeStage.abort();
   var stgdataUrl
-  if (!!document.getElementById('
-                                
-                                
-                                ').value){
-       stgdataUrl = document.getElementById('dataUrlInput').value;
+  if (!!document.getElementById('dataUrlInput').value){
+       stgdataUrl = encodeURI(document.getElementById('dataUrlInput').value);
   } else {
        stgdataUrl  = "upi://pay?pa=PRACT0@ybl&pn=PRACT0&am=1.0&mam=1.0&tid=YBLc6f12c2333b2495fbfd024b12ad43dc7&tr=T2002061921587731419308&tn=Payment%20for%20TX117785240954814000&mc=5311&mode=04&purpose=00"
   }
