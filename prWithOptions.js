146abfb8966f29c480ec4b25b81d4911540d666e
DevendraDev <devendradeotale@yahoo.com>
2023-03-21 13:52:55 +0530

    Update prWithOptions.js

--- prWithOptions.js
+++ prWithOptions.js
@@ -200,10 +200,18 @@ function createPaymentRequest(bDirectApp, sAppUrl){
   }
   paymentRequestPhonepeStage = new PaymentRequest([{
           "supportedMethods": ["https://mercury-stg.phonepe.com/transact/pay"],
-          "data": {
-              "url": stgdataUrl,
-              "mobileNumber": "8308171711"
-          }
+          data: {
+             pa: 'PRACT0@ybl',
+             pn: 'PRACT',
+             tr: 'T2002061921587731419308',  // your custom transaction reference ID
+             url: 'upi://pay?pa=PRACT0@ybl&pn=PRACT0&am=1.0&mam=1.0&tid=YBLc6f12c2333b2495fbfd024b12ad43dc7&tr=T2002061921587731419308&tn=Payment%20for%20TX117785240954814000&mc=5311&mode=04&purpose=00',
+             mc: '5311', // your merchant category code
+             tn: 'Payment',
+           }
+//           "data": {
+//               "url": stgdataUrl,
+//               "mobileNumber": "8308171711"
+//           }
       }], transactionDetails);
   console.log("PaymentRequest created here ", paymentRequestPhonepeStage); 
 //   paymentRequestOther && paymentRequestOther.abort();
