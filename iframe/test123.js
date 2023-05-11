3eca04fe57502130d3df71b9a4e5a2b5c07c3984
DevendraDev <devendradeotale@yahoo.com>
2023-05-11 07:42:06 +0530

    Create test123.js
    testing something

---
+++ iframe/test123.js
@@ -0,0 +1,20 @@
+
+//   function myFunction() {
+//     alert("Page is loaded");
+//   }
+  
+  function inIframe () {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+  }
+
+  function myFunction() {
+    console.log("inside body onload function");
+    if(inIframe()){
+      document.getElementById("signInButton").setAttribute("rel", "noreferrer");
+      document.getElementById("signInButton").setAttribute("target", "_blank");
+    }
+  }
